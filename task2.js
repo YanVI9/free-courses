3a168d2fca99c71d21efde6f45bc39d066213a29
Yan <it.yan@yandex.ru>
2018-05-30 00:35:22 +0300

    Добавлено условие смещения только символов английского алфавита, остальные без смещения

--- task2.js
+++ task2.js
@@ -10,7 +10,15 @@ if ( (inputStr.length >= 1 &&  inputStr.length <= 10000) || ( shift >= 0 && shif
 	for (var i = 0; i < inputStr.length; i++)
 	{
 		var charCod = inputStr[i].charCodeAt(0); //Получаем код символа из строки
-		outputStr = outputStr + String.fromCharCode(charCod + shift); //Добавляем к результату смещенный символ
+		
+		if ( (charCod >= 65 && charCod <= 90) || (charCod >= 97 && charCod <= 122) )
+		{
+			outputStr = outputStr + String.fromCharCode(charCod + shift); //Добавляем к результату смещенный символ
+		}
+		else
+		{
+			outputStr = outputStr + inputStr[i];
+		}
 	}
 }
 else
