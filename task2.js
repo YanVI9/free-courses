8cdbf3d4249aeedea72787fe361bb624cebd5534
Yan <it.yan@yandex.ru>
2018-05-29 23:58:49 +0300

    Проверка входных параметров на соответсвия условиям

--- task2.js
+++ task2.js
@@ -4,7 +4,14 @@
 var inputStr = process.argv[2];
 var shift = +process.argv[3];
 
-
+if ( (inputStr.length >= 1 &&  inputStr.length <= 10000) || ( shift >= 0 && shift <= 10000 ))
+{
+	
+}
+else
+{
+	process.stdout.write("Значение входных параметров не соответсвуют условиям");
+}
 
 
 //process.stdout.write(inputStr);
