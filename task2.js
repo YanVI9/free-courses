bf52af094cae9578ba8007ef78c72c178f95846d
Yan <it.yan@yandex.ru>
2018-05-30 00:15:51 +0300

    Шифрование любых символов

--- task2.js
+++ task2.js
@@ -6,14 +6,18 @@ var shift = +process.argv[3];
 
 if ( (inputStr.length >= 1 &&  inputStr.length <= 10000) || ( shift >= 0 && shift <= 10000 ))
 {
-	
+	var outputStr = "";
+	for (var i = 0; i < inputStr.length; i++)
+	{
+		var charCod = inputStr[i].charCodeAt(0); //Получаем код символа из строки
+		outputStr = outputStr + String.fromCharCode(charCod + shift); //Добавляем к результату смещенный символ
+	}
 }
 else
 {
 	process.stdout.write("Значение входных параметров не соответсвуют условиям");
 }
 
-
-//process.stdout.write(inputStr);
+process.stdout.write(outputStr);
 
 
