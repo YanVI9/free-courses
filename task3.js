6f187d17d4b3577b3c17b89a7aa2a52902db1d35
Yan <it.yan@yandex.ru>
2018-05-30 22:10:26 +0300

    Проверка результата и исправление ошибок.

--- task3.js
+++ task3.js
@@ -2,7 +2,7 @@ var
 	t2 = +process.argv.pop(), 
 	t1 = +process.argv.pop();
 
-if ( ( t1 >= 1	&& t1 <= 100000) || ( t2 >= 1	&& t2 <= 100000)
+if ( ( t1 >= 1	&& t1 <= 100000) || ( t2 >= 1	&& t2 <= 100000))
 {
 	var sum = t1 + t2; 
 	var h, m, s;
@@ -11,7 +11,7 @@ if ( ( t1 >= 1	&& t1 <= 100000) || ( t2 >= 1	&& t2 <= 100000)
 	h = Math.floor(m / 60);
 	m = m % 60;
 	
-	process.stdout.write(h + " часов " + m + " минут " + s " секунд");
+	process.stdout.write(h + " часов " + m + " минут " + s + " секунд");
 	
 }
 else
