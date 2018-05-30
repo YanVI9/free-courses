9010b7a6bdea113721e16cd64a0eff0928a24c62
Yan <it.yan@yandex.ru>
2018-05-30 22:07:20 +0300

    Вычисление секунд, минут и часов и суммы. Вывод в нужномсоответсвующем виде.

--- task3.js
+++ task3.js
@@ -4,6 +4,14 @@ var
 
 if ( ( t1 >= 1	&& t1 <= 100000) || ( t2 >= 1	&& t2 <= 100000)
 {
+	var sum = t1 + t2; 
+	var h, m, s;
+	s = sum % 60;
+	m = Math.floor(sum / 60);
+	h = Math.floor(m / 60);
+	m = m % 60;
+	
+	process.stdout.write(h + " часов " + m + " минут " + s " секунд");
 	
 }
 else
